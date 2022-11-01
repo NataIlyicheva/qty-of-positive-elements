5a6f6cc34fe03d6a78c363fd2ccf21421bf3137f
Natali <n7483205@ya.ru>
2022-11-01 21:57:32 +0700

    vals exceeding 0

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2    1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа через запятую");
+string str = Console.ReadLine()!;
+string[] array = str.Split(",");
+
+int[] resultArray = new int[array.Length];
+for (int i = 0; i < resultArray.Length; i++) 
+resultArray[i] = int.Parse(array[i]);
+
+int CountValLargerThanZero(int[] arr)
+{
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (arr[i] > 0)
+        counter++;
+        return counter;
+}
+int quantity = CountValLargerThanZero(resultArray);
+Console.WriteLine($"Количество чисел больше 0: {quantity}");
